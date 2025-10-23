3b193e70ce6d030ae69eff3f6b8abb0bfbb3f86e
Nikita Anton <aokidzy@MacBook-Air-Nikita.local>
2025-10-23 09:09:32 +0700

    Initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nikita Anton on 23.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
